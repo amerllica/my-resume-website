e868774b6df0b7a3669010925109ce7120aba5c8
amerllica <amerllica@gmail.com>
2023-06-18 03:13:50 +0330

    Fix a mistake

--- src/components/Sections/Header.tsx
+++ src/components/Sections/Header.tsx
@@ -12,7 +12,7 @@ export const headerID = 'headerNav';
 const Header: FC = memo(() => {
   const [currentSection, setCurrentSection] = useState<SectionId | null>(null);
   const navSections = useMemo(
-    () => [SectionId.About, SectionId.Resume, SectionId.Portfolio, SectionId.Recommendations, SectionId.Contact],
+    () => [SectionId.About, SectionId.Resume, SectionId.Recommendations, SectionId.Contact],
     [],
   );
 
