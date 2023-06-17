528357ed127c67d03dc57383b0aa171157f80086
amerllica <amerllica@gmail.com>
2023-06-17 23:03:11 +0000

    Deploying to gh-pages from @ amerllica/amerllica.github.io@a6f3f0ac8075bada69095ba76b5811cf6c80c158 🚀

--- _next/static/p2S_2cSDXHzCL2TdHNZKp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/357-39f15f4287d205a3.js","static/chunks/pages/index-3c3166671e7a837b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
