d73f19d12b01d12bab26aa9f9994c98b0a93c828
amerllica <amerllica@gmail.com>
2023-06-18 03:34:39 +0330

    Fix imports

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -8,13 +8,12 @@ import {
   SparklesIcon,
 } from '@heroicons/react/24/outline';
 
+import FacebookIcon from '../components/Icon/FacebookIcon';
 import GithubIcon from '../components/Icon/GithubIcon';
 // import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
-import FacebookIcon from '../components/Icon/FacebookIcon';
-
 import heroImage from '../images/header-background.webp';
 /*import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
