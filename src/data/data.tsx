1ce27f1c0b08585c412229e3fdae8107657c1a1c
amerllica <amerllica@gmail.com>
2023-09-26 02:10:12 +0330

    Update resume file and its link

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -81,7 +81,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '/assets/Amer_Lotfi_Orimi_-_June_2023.pdf',
+      href: '/assets/Amer_Lotfi_-_Sep_2023.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
